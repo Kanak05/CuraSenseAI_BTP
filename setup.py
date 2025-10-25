11af9921feb23cfe472676bafb74edf7523bd1ae
Kanak05 <kanak.nagar64@gmail.com>
2025-10-25 19:34:10 +0530

    requirement added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Kanak Nagar",
+    author_email="kanak.nagar64@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
